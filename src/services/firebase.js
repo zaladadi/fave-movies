c2d000003490085afc8ace775ce8a610c64fab6b
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 11:43:59 -0700

    Added firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,12 @@
-const config = {};
+const config = {
+    apiKey: 'AIzaSyAXZiP197HoWU3aXvSYn-DbPiMwH-PI1eQ',
+    authDomain: 'favorite-movies-3b1cf.firebaseapp.com',
+    databaseURL: 'https://favorite-movies-3b1cf.firebaseio.com',
+    projectId: 'favorite-movies-3b1cf',
+    storageBucket: 'favorite-movies-3b1cf.appspot.com',
+    messagingSenderId: '688529067835',
+    appId: '1:688529067835:web:1920a3eb9252f602'
+};
 
 firebase.initializeApp(config);
 
