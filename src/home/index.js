57211d8509f29e1ae11f19785faa17822dfd028b
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 13:05:42 -0700

    added auth to index.js

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,5 @@
 import App from './App.js';
-
+import '../utils/check-auth.js';
 const root = document.getElementById('app');
 const app = new App();
 root.appendChild(app.render());
---
+++ src/utils/check-auth.js
@@ -0,0 +1,8 @@
+import { auth } from '../services/firebase.js';
+
+auth.onAuthStateChanged(user => {
+    if(!user) {
+        window.location = '../../auth.html';
+    }
+});
+
