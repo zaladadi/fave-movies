7f2f25c64a41f14ec7e4458ec86ac9504574364b
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 13:35:39 -0700

    Created app component and linked to html

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,11 +1,37 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
+import { auth } from '../services/firebase.js';
+
+const ui = new firebaseui.auth.AuthUI(auth);
 
 class AuthApp extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const main = dom.querySelector('main');
+        dom.insertBefore(header.render(), main);
+
+        ui.start('#firebaseui-auth-container', {
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            ],
+            signInSuccessUrl: './',
+            credentialHelper: firebaseui.auth.CredentialHelper.NONE
+        });
+
+        return dom;
+    }
 
     renderTemplate() {
         return /*html*/`
         <div>
-            <main></main>
+            <main>
+                <p>Sign in to see your favorite movies</p>
+                <div id="firebaseui-auth-container"></div>
+            </main>
         </div>
             
         `;
