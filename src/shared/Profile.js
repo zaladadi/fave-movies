ed2786f3dbb6df9ada221901f6cc7ba80ace0ef7
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 14:52:26 -0700

    rendered profile and added event listener to sign out

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -1,12 +1,33 @@
 import Component from '../Component.js';
+import { auth } from '../../src/services/firebase.js';
 
 class Profile extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+
+        if(this.props.user) {
+            const button = dom.querySelector('button');
+            button.addEventListener('click', () => {
+                auth.signOut();
+            });
+        }
+
+        return dom;
+    }
+
     renderTemplate() {
+        const user = this.props.user;
+        
+        if(!user) {
+            return `<div></div>`;
+        }
+        const avatar = user.photoURL || '../../assets/default.png';
+
         return /*html*/`
             <div class="profile">
-                <img src="http://via.placeholder.com/50">
-                <span>Danny</span>
+                <img src="${avatar}">
+                <span>${user.displayName}</span>
                 <button>Sign Out</button>
             </div>
         `;
