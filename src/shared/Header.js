c23bf67ccea4dd0fc7deb820d8fa6eef30584216
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-03 13:36:23 -0700

    rendered profile into header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,6 +1,19 @@
 import Component from '../Component.js';
+import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+        
+        return dom;
+    }
 
     renderTemplate() {
         return /*html*/`
