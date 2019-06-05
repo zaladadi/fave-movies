1851d8c543aa1b6fbfa76df26bd96c29ef957ba9
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-04 17:23:52 -0700

    Removed console logs and whitespace

--- src/services/movieDB-api.js
+++ src/services/movieDB-api.js
@@ -3,22 +3,17 @@ import QUERY from '../QUERY.js';
 const URL = 'https://api.themoviedb.org/3/';
 const apiKey = 'api_key=eb6bb7c2bbf3c733ad68fc5e68499ee6';
 
-//   /discover/movie?sort_by=popularity.desc
-    
 const movieApi = {
 
     getMovies(queryProps) {
         let query; 
-        console.log(queryProps);
         if(!queryProps) {
             query = 'movie/popular';
         }
         else {
             query = 'search/movie?' + QUERY.stringify(queryProps);
         }
-        console.log(query);
         const url = `${URL}${query}&${apiKey}`;
-        console.log(url);
         return fetch(url)
             .then(response => response.json());
     }
