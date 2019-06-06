3152883d3bd19ece8376234dd55cdcb3365744a6
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-06 10:38:11 -0700

    Changed location of back slash in api URL

--- src/services/movieDB-api.js
+++ src/services/movieDB-api.js
@@ -1,7 +1,8 @@
 import QUERY from '../QUERY.js';
 
-const URL = 'https://api.themoviedb.org/3/';
+const URL = 'https://api.themoviedb.org/3';
 const apiKey = 'api_key=eb6bb7c2bbf3c733ad68fc5e68499ee6';
+
 
 const movieApi = {
 
@@ -13,13 +14,13 @@ const movieApi = {
         else {
             query = 'movie/popular?';
         }
-        const url = `${URL}${query}&${apiKey}`;
+        const url = `${URL}/${query}&${apiKey}`;
         return fetch(url)
             .then(response => response.json());
     },
 
     getMovieById(id) {
-        const detailUrl = `${URL}movie/${id}?${apiKey}`;
+        const detailUrl = `${URL}/movie/${id}?${apiKey}`;
         return fetch(detailUrl)
             .then(response => response.json());
     }
