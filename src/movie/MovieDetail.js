47e5bfcb7037cf24412f827a24b66452b8ef0b05
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-06 13:32:27 -0700

    rendered usermovieref into movie detail.html

--- src/movie/MovieDetail.js
+++ src/movie/MovieDetail.js
@@ -1,6 +1,32 @@
 import Component from '../Component.js';
+import Favorite from '../shared/Favorite.js';
+import { setFavorite, getUserMovieFavoriteRef } from '../services/actions.js';
 
 class MovieDetail extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const movie = this.props.movie;
+
+        if(movie) {
+            const userMovieRef = getUserMovieFavoriteRef(movie.id);
+            const favorite = new Favorite({
+                isFavorite: false,
+                onClick: (makeFavorite) => {
+                    setFavorite(movie, makeFavorite);
+                }
+            });
+
+            userMovieRef.on('value', snapshot => {
+                const isFavorite = Boolean(snapshot.val());
+                favorite.update({ isFavorite });
+            });
+
+            dom.appendChild(favorite.render());
+        }
+
+        return dom;
+    }
 
     renderTemplate() {
         const movie = this.props.movie;
@@ -14,12 +40,14 @@ class MovieDetail extends Component {
             imgPath = '../../assets/default-movie.png';
         }
         return /*html*/`
-            <section>
-                <h2>${movie.title}</h2>
+            <section class="movie-detail">
                 <img src="${imgPath}${movie.poster_path}">
-                <p>${movie.release_date}</p>
-                <p>${movie.overview}
-                </p>
+                <section class="text">
+                    <h2>${movie.title}</h2>
+                    <p class="release-date">${movie.release_date}</p>
+                    <p>${movie.overview}</p>
+                </section>
+                <div class="favorite-container"></div>
             </section>
         `;
     }
