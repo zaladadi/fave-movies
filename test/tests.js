c32022ba86a980425d762c7c23dc50921318b68a
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-05 17:56:22 -0700

    Created tests for MovieDetail component

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,7 @@
 import './html-equal.js';
 import './MovieItem.test.js';
 import './Profile.test.js';
+import './MovieDetail.test.js';
 import { app } from '../src/services/firebase.js'; 
 
 QUnit.done(() => {
