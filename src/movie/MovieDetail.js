33ccb9863fcd5d127f6e8eea9cfe2dfa803c1edc
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-05 17:53:35 -0700

    Created MovieDetail component

---
+++ src/movie/MovieDetail.js
@@ -0,0 +1,21 @@
+import Component from '../Component.js';
+
+class MovieDetail extends Component {
+
+    renderTemplate() {
+        const movie = this.props.movie;
+
+        let imgPath = 'https://image.tmdb.org/t/p/w500';
+        return /*html*/`
+            <section>
+                <h2>${movie.title}</h2>
+                <img src="${imgPath}${movie.poster_path}">
+                <p>${movie.release_date}</p>
+                <p>${movie.overview}
+                </p>
+            </section>
+        `;
+    }
+}
+
+export default MovieDetail;
