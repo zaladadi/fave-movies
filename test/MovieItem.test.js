3291cc7ea216821ff84f75a09ef822556d72bebe
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-07 10:44:24 -0700

    Fixed expected in test

--- test/MovieItem.test.js
+++ test/MovieItem.test.js
@@ -17,6 +17,7 @@ test('return empty div if movie is null', assert => {
     //assert
     const expected = `
         <li class="movie-item">
+            <div class="favorite-container"></div>
             <a href="./movie.html?id=${movie.id}"><h2 id="title">Fight Club</h2></a>
             <img id="poster" src="https://image.tmdb.org/t/p/w500/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg">
         </li>
