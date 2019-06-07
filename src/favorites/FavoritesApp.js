bd7db65c04bc8bd5190aa5866b10fae168a1acbc
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-07 09:26:45 -0700

    Imported QUERY utility and checks URL to see if user is present

--- src/favorites/FavoritesApp.js
+++ src/favorites/FavoritesApp.js
@@ -1,6 +1,7 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import MovieList from '../shared/MovieList.js';
+import QUERY from '../QUERY.js';
 import { auth, userFavoritesRef } from '../services/firebase.js';
 
 class FavoritesApp extends Component {
@@ -14,8 +15,11 @@ class FavoritesApp extends Component {
         const movieList = new MovieList({ movieData: [] });
         main.appendChild(movieList.render());
 
+        const query = QUERY.parse(window.location.search);
+        const uid = query.uid ? query.uid : auth.currentUser.uid;
+
         userFavoritesRef
-            .child(auth.currentUser.uid)
+            .child(uid)
             .on('value', snapshot => {
                 const value = snapshot.val();
                 const movieData = value ? Object.values(value) : [];
