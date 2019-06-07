ba42128b23641e2757e48596e6dfdc18b1f7154b
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-07 09:25:41 -0700

    Created dynamic link to user favorites

--- src/movie/UserItem.js
+++ src/movie/UserItem.js
@@ -5,7 +5,7 @@ class UserItem extends Component {
     renderTemplate() {
         const user = this.props.user;
         return /*html*/`
-            <a href="#">
+            <a href="./favorites.html?uid=${user.uid}">
                 <li class="user-item">
                     <img src="../../assets/default.png">
                     <p>${user.displayName}</p>
